564042aacc1403a2bf7b6ad8d8739cdbebc83ba2
Brandon <brandon@Sarahs-MacBook-Air.local>
2017-09-02 17:24:47 -0400

    python program v1 - initial commit

---
+++ assignment_01.py
@@ -0,0 +1,47 @@
+# python 2
+
+import sys
+import cv2
+
+#Check and Read arguments
+if len(sys.argv) < 4:
+   print "ERROR: Missing Arguments"
+   print "\tHOW: python %s image.jpg name last_name" % (sys.argv[0])
+   sys.exit(1)
+
+input_image = sys.argv[1]
+name = sys.argv[2]
+last_name = sys.argv[3]
+
+#Read image
+#COLOR:
+img = cv2.imread(sys.argv[1],1)
+#GRAYSCALE:
+#img = cv2.imread(sys.argv[1],0)
+
+#Write image
+str_to_write = name + " " + last_name
+font = cv2.FONT_HERSHEY_PLAIN
+color = (0,0,255) #Red because OpenCV is BGR
+
+#COLOR
+rows,cols,depth = img.shape
+#GRAYSCALE:
+#rows,cols = img.shape
+
+cv2.putText(img,str_to_write,(rows/10,cols/10),font,2,color,6,cv2.LINE_AA)
+#cv2.putText(img,str_to_write,(rows/10,cols/10),font,2,color,6,cv2.CV_AA)
+
+#Show image and wait
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+
+# wait for ESC key to exit
+if k == 27:    
+    cv2.destroyAllWindows()
+
+# wait for 's' key to save and exit
+elif k == ord('s'):
+    output_file_name = last_name + "_output.jpg"
+    cv2.imwrite(output_file_name,img)
+    cv2.destroyAllWindows()
